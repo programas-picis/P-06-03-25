c64cc33e5b93689948c38638a434c2969a3b1c87
Franccesco Santofimio <liddigital@picisestrategica.com>
2025-03-07 07:28:45 -0500

    Beginning

---
+++ Views/footer.php
@@ -0,0 +1,54 @@
+<div class="layout-page">
+  <footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl">
+      <div
+        class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+        <div class="mb-2 mb-md-0">
+          &#169;
+          <script>
+            document.write(new Date().getFullYear());
+          </script>
+          , made with ❤️ by
+          <a href="https://themeselection.com" target="_blank" class="footer-link">ThemeSelection</a>
+        </div>
+        <div class="d-none d-lg-inline-block">
+          <a
+            href="https://themeselection.com/item/category/admin-templates/"
+            target="_blank"
+            class="footer-link me-4">Admin Templates</a>
+
+          <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
+          <a
+            href="https://themeselection.com/item/category/bootstrap-admin-templates/"
+            target="_blank"
+            class="footer-link me-4">Bootstrap Dashboard</a>
+
+          <a
+            href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation/"
+            target="_blank"
+            class="footer-link me-4">Documentation</a>
+
+          <a
+            href="https://github.com/themeselection/sneat-bootstrap-html-admin-template-free/issues"
+            target="_blank"
+            class="footer-link">Support</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</div>
+
+
+<!-- Core JS -->
+
+<script src="/Intranet_amaro/assets/vendor/libs/jquery/jquery.js"></script>
+<script src="/Intranet_amaro/assets/vendor/libs/popper/popper.js"></script>
+<script src="/Intranet_amaro/assets/vendor/js/bootstrap.js"></script>
+<script src="/Intranet_amaro/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="/Intranet_amaro/assets/vendor/js/menu.js"></script>
+<script src="/Intranet_amaro/assets/vendor/libs/apex-charts/apexcharts.js"></script>
+<script src="/Intranet_amaro/assets/js/main.js"></script>
+<script src="/Intranet_amaro/assets/js/dashboards-analytics.js"></script>
+
+<!-- Place this tag before closing body tag for github widget button. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
---
+++ Views/login.php
@@ -0,0 +1,117 @@
+<!doctype html>
+
+<html
+  lang="en"
+  class="layout-wide customizer-hide"
+  data-assets-path="../assets/"
+  data-template="vertical-menu-template-free">
+
+
+<body>
+  <!-- Content -->
+
+  <div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+      <div class="authentication-inner">
+        <!-- Register -->
+        <div class="card px-sm-6 px-0">
+          <div class="card-body">
+            <!-- Logo -->
+            <div class="app-brand justify-content-center">
+              <a href="index.html" class="app-brand-link gap-2">
+                <span class="app-brand-logo demo">
+                <img src="/intranet_amaro/assets/img/favicon/comunidades.png" alt="Logo" width="36" height="38" />
+                </span>
+                <span class="app-brand-text demo text-heading fw-bold">Grupo Amaro</span>
+              </a>
+            </div>
+
+            <!-- /Logo -->
+            <h4 class="mb-1">Bienvenido a la Intranet!👋</h4>
+            <p class="mb-6">Inicia sesión en tu cuenta y comienza a navegar en la Intranet corporativa.</p>
+
+            <form id="formAuthentication" class="mb-6" action="index.html">
+              <div class="mb-6">
+                <label for="email" class="form-label">Cedula</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="email"
+                  name="email-username"
+                  placeholder="Ingrese su cedula"
+                  autofocus />
+              </div>
+              <div class="mb-6 form-password-toggle">
+                <label class="form-label" for="password">Contraseña</label>
+                <div class="input-group input-group-merge">
+                  <input
+                    type="password"
+                    id="password"
+                    class="form-control"
+                    name="password"
+                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                    aria-describedby="password" />
+                  <span class="input-group-text cursor-pointer"><i class="icon-base bx bx-hide"></i></span>
+                </div>
+              </div>
+              <div class="mb-6">
+                <label for="email" class="form-label">Empresa</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="email"
+                  name="email-username"
+                  placeholder=""
+                  autofocus />
+              </div>
+              <div class="mb-8">
+                <div class="d-flex justify-content-between">
+                  <a href="auth-forgot-password-basic.html">
+                    <span>Olvidaste tu contraseña?</span>
+                  </a>
+                </div>
+              </div>
+              <div class="mb-6">
+                <button class="btn btn-primary d-grid w-100" type="submit">Iniciar Sesion</button>
+              </div>
+            </form>
+
+            <!-- <p class="text-center">
+              <span>New on our platform?</span>
+              <a href="auth-register-basic.html">
+                <span>Create an account</span>
+              </a>
+            </p> -->
+          </div>
+        </div>
+        <!-- /Register -->
+      </div>
+    </div>
+  </div>
+
+  <!-- Core JS -->
+
+  <script src="/Intranet_amaro/assets/vendor/libs/jquery/jquery.js"></script>
+
+  <script src="/Intranet_amaro/assets/vendor/libs/popper/popper.js"></script>
+  <script src="/Intranet_amaro/assets/vendor/js/bootstrap.js"></script>
+
+  <script src="/Intranet_amaro/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+  <script src="/Intranet_amaro/assets/vendor/js/menu.js"></script>
+
+  <!-- endbuild -->
+
+  <!-- Vendors JS -->
+
+  <!-- Main JS -->
+
+  <script src="/Intranet_amaro/assets/js/main.js"></script>
+
+  <!-- Page JS -->
+
+  <!-- Place this tag before closing body tag for github widget button. -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+
+</html>
